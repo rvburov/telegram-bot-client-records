6cfeb98c915fe579c64ef7e6fd0fd79b12c99dc7
romanburov <romanburov@MacBook-Pro-romanburov.local>
2024-11-22 16:13:00 +0300

    Version polling

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 import os
-from fastapi import FastAPI, Request
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
 from dotenv import load_dotenv
 
 # Загружаем переменные окружения
